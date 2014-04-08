ff1b38fca88097ae43f16507f09d1053d205198b
Khalid Richards <columbiakrich@gmail.com>
2014-04-07 20:19:49 -0400

    Added sign-up controller

--- grails-app/controllers/itinerate/SignupController.groovy
+++ grails-app/controllers/itinerate/SignupController.groovy
@@ -4,21 +4,35 @@ class SignupController {
 
     def index() {
     	//1. Get the JSONified version of the current (pending) user's itinerary.
-    	pendingItinerary = /*stuff*/;
-
-
-    	//2. 
-
+    	pendingItinerary = /*some JSON object*/;
+    	[itinerary: pendingItinerary]
     }
-
+    
     def submit_form() {
-    	//1. Store user information in the database
+    	def userId = 0
 
-    	//2. Redirect user to page with flash message
+    	if(params.username != null) {
+    		userId = User.createUserByEmailAndUname(params.email, params.username, params.password)			
+		} else {
+			userId = User.createUserByEmail(params.email, params.password)
+		}
 
-    }
-
-    def registration_success() {
-    	//1. Display flash message and after X seconds redirect user to home page, passing along the itinerary.
+		if(userId > 0){
+			def user = User.getUserFromId(userId)
+			user.iternary = /* some JSON stuff */
+			session.userId = userId
+			redirect(controller:'itinerary', action: 'print')
+			return
+		}
+			// notify user that this username has already been used
+		else if(userId == -1){
+			def errorMessage = "This user exists, try again!"
+			redirect(controller:'subscription', action: 'index', params: [errorMessage:errorMessage])
+		}
+			// notify error occurred
+		else{
+			def errorMessage = "Please enter a valid username/password"
+			redirect(controller:'subscription', action: 'index', params: [errorMessage:errorMessage])
+		}
     }
 }
