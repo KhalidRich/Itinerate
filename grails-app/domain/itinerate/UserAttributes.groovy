84a26aace10f8ee94fadc0bbe5f95dfa7a9030ed
kugurst <ma2799@gmail.com>
2014-04-07 20:15:04 -0400

    Added a few more constraints.
    UserAttributes should be fine for now.

--- grails-app/domain/itinerate/UserAttributes.groovy
+++ grails-app/domain/itinerate/UserAttributes.groovy
@@ -6,10 +6,12 @@ class UserAttributes
     String profilePicturePath
     Boolean verified = false
     Date lastPayed = new Date(0)
+    Date previousLastPayed = new Date(-1)
 
     static belongsTo = [user: User]
     static constraints = {
         verified nullable: false
         lastPayed nullable: false
+        previousLastPayed nullable: false
     }
 }
