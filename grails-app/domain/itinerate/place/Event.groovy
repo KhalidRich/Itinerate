2a57fe4d484b3bf8a946fc03c5c164d3ce53fd32
kugurst <ma2799@gmail.com>
2014-04-09 18:05:43 -0400

    The addresses in the old database are not in a form parseable by the Address class.
    In particular, they usually indicate streets or street intersections, which requires geodecoding to get an actual mailable address. For this reason, we'll just store the strings for now, and decode them later.

--- grails-app/domain/itinerate/place/Event.groovy
+++ grails-app/domain/itinerate/place/Event.groovy
@@ -1,7 +1,28 @@
 package itinerate.place
 
-class Event {
+import itinerate.plan.Day
+import itinerate.plan.Holiday
 
+class Event
+{
+    String name
+    String address
+    String telephoneNumber
+    String website
+    Price pricing
+    
+    String startMonth
+    String endMonth
+    
+    Boolean ticketsRequired
+    String ticketLink
+    
+    List ticketExceptions
+    List picturePaths
+    
+    static belongsTo = [day: Day]
+    static hasMany = [tags: String, hours: Hours, holidays: Holiday, ticketExceptions: String, picturePaths: String, ratings: Rating]
+    static embedded = ['tags', 'ticketExceptions', 'picturePaths']
     static constraints = {
     }
 }
--- src/groovy/itinerate/EventDatabaseBuilder.groovy
+++ src/groovy/itinerate/EventDatabaseBuilder.groovy
@@ -1,9 +1,9 @@
 package itinerate
 
-import itinerate.plan.Event
+import itinerate.place.Event
 import itinerate.place.Address
 
-def databaseFile = new File("../../Database Categories.txt")
+def databaseFile = new File("Database Categories.txt")
 
 def addTags(event, tags)
 {
@@ -29,9 +29,11 @@ databaseFile.eachLine {
                 // Assign the tags
                 addTags(event, columns[2..5])
                 // Assign the address
-                if (!columns[2].trim().isEmpty()) {
-                    Address address = Address.getAddressFromString(columns[2])
-                    if (!address.isAttached)
+                if (!columns[6].trim().isEmpty()) {
+                    Address address = Address.getAddressFromString(columns[6])
+                    if (address == null) {
+                        println "${columns[6]} does not yeild a valid address!"
+                    } else if (!address.isAttached)
                         address.attach()
                     event.address = address
                 }
