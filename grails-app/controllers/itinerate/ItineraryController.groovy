35a5a5954066601a5e75e59c17397dc3945e3025
asdas209 <asdas204@gmail.com>
2014-04-10 20:10:47 -0400

    Adding itinerary review page

--- grails-app/controllers/itinerate/ItineraryController.groovy
+++ grails-app/controllers/itinerate/ItineraryController.groovy
@@ -1,5 +1,9 @@
 package itinerate
 
+import itinerate.place.Address
+import itinerate.place.Price
+import itinerate.plan.Event
+
 class ItineraryController {
 
     def index() { 
@@ -10,5 +14,18 @@ class ItineraryController {
 	}
 	def Show() {
 		redirect(controller:"ShowController",action:"index")
+	}
+	
+	def review() {
+		// TODO: Get dynamic data derived from userId and Id number
+		// For Now I am creating dummy data in order to test
+		Event event1 = new Event([name:"Play Pen", 
+								  telephoneNumber : "2034302493024",
+								  website : "www.goole.com",
+								  ticketsRequired : true
+								  ])
+		
+
+			
 	}
 }
---
+++ grails-app/views/itinerary/review.gsp
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<header>
+<g:render template="/layouts/header" />
+<g:render template="/layouts/navbar" />
+</header>
+<body>
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Panel heading</div>
+  <div class="panel-body">
+    <p>...</p>
+  </div>
+
+  <!-- Table -->
+  <table class="table">
+    ...
+  </table>
+</div>
+</body>
+</html>
