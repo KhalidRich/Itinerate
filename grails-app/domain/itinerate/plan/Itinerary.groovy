84347a12054110593e35394ff3b8df76459b8441
kugurst <ma2799@gmail.com>
2014-04-11 14:38:40 -0400

    Added a createdAt property to Itinerary

--- grails-app/domain/itinerate/plan/Itinerary.groovy
+++ grails-app/domain/itinerate/plan/Itinerary.groovy
@@ -7,11 +7,14 @@ class Itinerary
 	String name
 	String location = "Philadelphia" // no other location for now
 
+	Date createdAt = new Date()
+
 	List days
 
     static belongsTo = [user: User]
     static hasMany = [days: Day]
     static constraints = {
     	name nullable: false
+    	createdAt nullable: false
     }
 }
